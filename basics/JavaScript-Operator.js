44ab6500bb4c0dbb1eb1c6d9c43846767bf8b09e
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-02-13 22:39:48 +0600

    add operator

---
+++ basics/JavaScript-Operator.js
@@ -0,0 +1,8 @@
+"use strict"
+
+// Operators are for Mathematical and Logical Computations
+
+// Arithmetic operators
+// Arithmetic operators perform arithmetic on numbers (literals or variables).
+
+let sum = 10 + 20 ;
