90ce7a9c784fa99b0f6131e14877ebb01a27fb1e
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-02-13 23:27:06 +0600

    js- operator-part_1

--- basics/JavaScript-Operator.js
+++ basics/JavaScript-Operator.js
@@ -5,4 +5,64 @@
 // Arithmetic operators
 // Arithmetic operators perform arithmetic on numbers (literals or variables).
 
+
+
+// Addition operator (+)
 let sum = 10 + 20 ;
+console.log(sum)
+
+
+let price = 120 ,
+    deliveryCharg = 10 ;
+
+const totalPrice = price + deliveryCharg ;
+
+console.log(totalPrice);
+
+    //   The + can also be use to add  strings:
+    let firstName = "hunter";
+    let lastName = "max";
+    let fullName = firstName + ' ' + lastName ;  //(+' ') space
+    console.log(fullName);
+   
+    let x = "this is x later";
+    x += " & the next later is y";
+    console.log(x)
+  
+    // adding number and string
+    let str = "this is hunter ";
+    let num = 100 ;
+    let sumS = str + num ;
+    console.log( sumS);   // console.log(typeof sumS); = string
+
+
+// Subtraction operator (-)
+let sub = 50 - 40 ;
+console.log(sub);
+
+
+let product = 100 ;
+let discoutPrice = 40 ;
+
+     // console.log(product - discoutPrice); or
+let total = product - discoutPrice ;
+
+console.log("total product price: " + total);
+
+
+
+
+// 	Multiplication operators (*)
+ let mult = 5 * 10 ;
+ console.log(mult);
+
+
+
+//	Division operators (/)
+let result = 50 / 2 ;
+console.log(result);
+
+
+
+
+// Modulus (Remainder) operators (%)
