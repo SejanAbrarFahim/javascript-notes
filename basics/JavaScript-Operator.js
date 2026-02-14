eaa3701c00343fb88ac44bb521c9ca632fed7b8b
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-02-14 22:46:06 +0600

    update basic and add advance folder

--- basics/JavaScript-Operator.js
+++ basics/JavaScript-Operator.js
@@ -66,3 +66,123 @@ console.log(result);
 
 
 // Modulus (Remainder) operators (%)
+  let math = 10 % 3 ;
+  console.log(math);
+
+
+
+//  Assignment operators (= , += , -+ etc)
+// Assignment operators assign values to JavaScript variables. 
+
+let a = 5 ;
+    a += 10;
+console.log(a)
+
+
+
+// Unary operators  
+/* 01. (+x) unary plus - convert a value into a number .
+   02. (-x) unary Minus - convert a value into a number and negate it .
+   03. (++x) Increment Operator - Add one to the value .
+   04. (--x) Decrement operator - Subtract one from the value .
+   05. (x++) increment operator - Add one to the value .
+   06. (x--) decrement operator - Subtract one of the value.*/
+
+ //    (+x)
+   let xo = "10" ;
+   let yo = +xo;
+   console.log(yo);   //typeof = number 
+
+ // (-x)
+  let xn = 10 ;
+  let yn = -xn ;
+  console.log(yn);
+
+ //  (++x) or (x++)
+   let age = 20 ;
+       ++age ;
+    console.log(age);
+
+ // (--x) or (x--)
+  let weight = 62 ;
+     --weight; 
+  console.log(weight);
+
+
+
+
+// Comparison operators (true or fals)
+ /* 01. < - less than .   
+    02. > - greater than .
+    03. <= - less than or equal to .
+    04. >= - greater than or equal to .
+    05. != - not equal to .
+    o6. = - equal.
+ */
+  
+ //   (<)
+   let ao = 10 ;
+   let bo = 20 ;
+   console.log(ao < bo);
+
+ //   (>)
+   let aob = 5 ;
+   let boa = 20 ;
+   console.log(aob > boa);
+
+ // (<=)
+   let bob = 5 ;
+   let dbob = 5;
+   console.log(dbob <= bob);
+
+ // (>=)
+   let dBob = 5;
+   let BoB = 4 ;
+   console.log(dBob >= BoB);
+
+ // (!=)
+   let got = 7 ;
+   let GoT = 10 ;
+   console.log(got != GoT);
+  
+ // (==)  or  (===)- check data type
+   let type = 5;
+   let typeA = 5;
+   console.log(type == typeA);
+
+
+
+//   logical operator (true or fals)
+ /* 01. (!) logical NOT .
+    02. (||) logical OR .
+    03. (&&) logical AND. */
+
+ // (!)
+    let eligible = false ;
+    let noteligble = true ;
+    console.log(!eligible);
+    console.log(!noteligble);
+
+ // (||)
+   let Or = true ;
+   let ort = false ;
+   console.log(Or || orf); //true
+
+   let orF = false ;
+   let orf = false ;
+   console.log(orF || orf); //false
+
+ // (&&)  
+    let typeAn = true ;
+    let typeBl = true ;
+    console.log( typeAn && typeBl);
+
+
+
+// exponentialion operator (**) - power
+
+  let rasult1 = 10 ;
+  let result2 = 2 ;
+  console.log(rasult1 ** result2); 
+
+ 
---
+++ basics/JavaScript_Reserved_Word.js
@@ -0,0 +1,22 @@
+// JavaScript Reserved Words 
+  // In JavaScript you cannot use these reserved words as variables, labels, or function names: 
+
+  /*
+  abstract,	arguments,	async*,	await *,
+  boolean,	break,	byte,	case,
+  catch,	char,	class *,	const *,
+  continue,	debugger,	default,	delete,
+  do,	  double,	  else,	  enum *,
+  eval,	export *,	extends *,	false,
+  final,	 finally,	float,	for,
+  function,	goto,	if,	implements,
+  function,	import *,	in,	instanceof,
+  int,	interface,	let *,	long,
+  native,	new	null,	package,
+  private,	protected,	public,	return,
+  short,	static,	super *,	switch,
+  synchronized,	 this,	throw,	throws,
+  transient,	true,	try,	typeof,
+  using *,	var	void,	volatile,
+  while	,  with, 	yield etc ......
+  */
