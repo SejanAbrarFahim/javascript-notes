ae15bbc3b6d3d7f538d524f30cc820478b99d889
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-02-18 11:50:46 +0600

    Create Math_object.js

---
+++ Condition & control flow/Math_object.js
@@ -0,0 +1,50 @@
+// Number to integer
+
+ // Math.round(x) - Return x rounded to its nearest integer .
+    let math = Math.round(4.4);
+    console.log(math); // 4
+
+    let math2 = Math.round(4.7);
+    console.log(math2); // 5
+
+    let Math3 = Math.round(4.5);
+    console.log(Math3); // 5
+
+
+
+  // Math.ceil(x) - Returns x rounded up to its nearest integer .
+    let MathCeil = Math.ceil(3.2);
+    console.log(MathCeil); // 4
+
+    let mathCeil = Math.ceil(0.8);
+    console.log(mathCeil); // 1
+
+
+
+  // Math.floor(x) - Returns x rounded down to its nearest integer
+     let mathFloor = Math.floor(5.6);
+     console.log(mathFloor); //5
+
+     let MathFloor = Math.floor(5.4);
+     console.log(MathFloor); // 5
+
+
+
+  // Math.trunc(x) - Returns the integer part of x (new in Ex6)
+    let trunc = Math.trunc(1.9);
+    console.log(trunc); // 1
+
+    let Trunc = Math.trunc(-1.7);
+    console.log(Trunc); // -1
+
+
+  
+  // Math.sign(x) - Returns if x is negative, null or positive (1 or 0 or -1)
+    let sign = Math.sign(-4);
+    console.log(sign); // -1
+   
+    let Sing = Math.sign(0);
+    console.log(Sing);  // 0
+
+    let number = Math.sign(2);
+    console.log(number); //1
