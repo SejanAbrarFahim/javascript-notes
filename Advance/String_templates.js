f7a169fb82b26954ced0e742672e576638c3bd4e
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-02-22 12:51:58 +0600

    Update String_templates.js

--- Advance/String_templates.js
+++ Advance/String_templates.js
@@ -4,20 +4,21 @@
       03. Template Strings allow multiline strings . */
 
   let text = `loerm `;
-  console.log(text);
+  console.log(text); // lorem
 
   let text1 = `Hello "programmer"`;
-  console.log(text1);
+  console.log(text1); // Hello "programmer"
 
   let text2 = `Hello programmer 
   "what's up"`;
-  console.log(text2);
+  console.log(text2); /* Hello programmer
+                       "What's up" */
 
   let firstName = "lorem";
   let lastName = "epsom";
-  console.log(`my name is ${firstName}, ${lastName}`);
+  console.log(`my name is ${firstName} ${lastName}`); // my name is lorem  epsom
 
 
   let sum = 20 ; 
   let sum1 = 30;
-  console.log(`This is javascript expressions: ${sum + sum1}`);
+  console.log(`This is javascript expressions: ${sum + sum1}`); // This is javascript expressions: 50
