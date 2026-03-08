d28ba3d940e543ac659e80122e34b00a3f4f4712
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-03-08 15:55:45 +0600

    Update Default function parameters, Rest parameter & Spread operator.js

--- ES6/Default function parameters, Rest parameter & Spread operator.js
+++ ES6/Default function parameters, Rest parameter & Spread operator.js
@@ -3,5 +3,24 @@
     console.log(message)
   };
 
-  say(); 
+  say(); // hello programmer
+
+
+
+
+// Rest parameter
+ function number(a,b,c,d){
+   console.log(a,b,c,d);
+ };
+
+ number(2,3,4,5,5); // 2 3 4 5
+
+
+ function Rest(a,b,c,...argu){
+  console.log(a,b,c,argu)
+ }
   
+Rest(1,23,342,2342,23423,2,3,4,4,); //1 23 342 [ 2342, 23423, 2, 3, 4, 4 ]
+
+
+
