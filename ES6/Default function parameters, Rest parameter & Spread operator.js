6883dae3adab1069ac3752d87c1e8b08d2063b41
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-03-08 16:14:44 +0600

    Update Default function parameters, Rest parameter & Spread operator.js

--- ES6/Default function parameters, Rest parameter & Spread operator.js
+++ ES6/Default function parameters, Rest parameter & Spread operator.js
@@ -16,11 +16,32 @@
  number(2,3,4,5,5); // 2 3 4 5
 
 
- function Rest(a,b,c,...argu){
-  console.log(a,b,c,argu)
+ function Rest(a,b,c,...argo){
+  console.log(a,b,c,argo)
  }
   
 Rest(1,23,342,2342,23423,2,3,4,4,); //1 23 342 [ 2342, 23423, 2, 3, 4, 4 ]
 
 
 
+
+
+// spread operator 
+  /* 01. Constructing array literal -
+         The spread operator allows you to insert another array into the initialized array when you construct an array using the literal form. 
+     02. Concatenating array -
+         Also, you can use the spread operator to concatenate two or more arrays.
+     03. Copying an array.
+         In addition, you can copy an array instance by using the spread operator.
+  */
+
+  // 01. Construct
+     let valueOne = [1,2,3];
+     let valueTwo = [...valueOne,6,7,8]; 
+     console.log(valueTwo);  // [ 1, 2, 3, 6, 7, 8 ]
+
+    
+  // 02. Concatenating
+
+     let teacher = ["teacher1"]
+     
