bd7c68fbe295880a57b5b9d83162bb56f985b08c
Sejan Abrar Fahim <sejan62007@gmail.com>
2026-03-09 16:10:53 +0600

    Add Object literal

---
+++ ES6/Object literal syntax.js
@@ -0,0 +1,56 @@
+// Object literal syntax extensions in ES6
+  // step 1
+    function SayHI(userName,UserInfo){
+        return {
+            userName,
+            UserInfo
+        }
+    }
+
+    console.log(SayHI("lorem","Programmer")); // { userName: 'lorem', UserInfo: 'Programmer' }
+
+
+  // step 2
+    let Message = "Hi , programmer";
+       userName = "lorem";
+
+    const info = {
+        Message,
+        userName
+    }
+
+    console.log(info); // { Message: 'Hi , programmer', userName: 'lorem' }
+
+   
+  // step 3
+     let name = "lorem"
+     let Info = {
+        [name] : "He is a Programmer"
+      }
+
+     console.log(info); // { Message: 'Hi , programmer', userName: 'lorem' }
+     console.log(name); // lorem
+     console.log(info["lorem"]); // lorem
+
+
+  // step 4
+     let server = {
+        namer : "server One",
+        restart(){
+            console.log("This is concise Method, and it's working perfectly");
+        }
+     }
+
+     console.log(server.restart()); // This is concise Method, and it's working perfectly
+
+
+
+     let serverTwo = {
+        namer : "server two",
+        "restart"(){
+            console.log("This is concise Method");
+        }
+     }
+
+
+     console.log(serverTwo["restart"]()); // This is concise Method
